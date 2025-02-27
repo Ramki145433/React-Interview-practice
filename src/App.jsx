7b4340422a1bb31d9f5840e0d5cb740e710a192a
Rama Krishna <injarapuramakrishna55@gmail.com>
2025-02-27 11:05:42 +0530

    chess question

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./Components/Question/Question"
 import Question from "./Components/Question/Question"
+import Chess from "./Components/Chess/Chess"
 export default function App() {
   return (
     <div>
-      <Question />
+      <Chess />
     </div>
   )
 }
---
+++ src/Components/Chess/Chess.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react"
+import "./Chess.css"
+export default function Chess({BoardSize = 8}) {
+    let chessMatrix = Array(BoardSize).fill(0).map((_,rowIndex) => Array(BoardSize).fill(0)
+    .map((_,colIndex) => ((rowIndex + colIndex) % 2 === 0 ? "black" : "white")))
+    console.log(chessMatrix)
+    const [selectedPosition, setSelectedPosition] = useState(null);
+    function getBishopMoves(row, col) {
+        let moves = new Set();
+        moves.add(`${row}-${col}`)
+        for(let i = 1; i <= BoardSize; i++) {
+            // for --
+            if((row - i) >= 0 && (col - i) >= 0) moves.add(`${row-i}-${col-i}`)
+            // for -+
+            if((row - i) >= 0 && (col + i) < BoardSize) moves.add(`${row-i}-${col+i}`)
+            // for +-
+            if((row + i) < BoardSize && (col - i) >= 0) moves.add(`${row+i}-${col-i}`)
+            // for ++
+            if((row + i) < BoardSize && (col + i) < BoardSize) moves.add(`${row+i}-${col+i}`)
+        }
+    console.log(moves)
+    return moves
+    }
+    const handleClick = (r,c) => {
+        console.log(r,c)
+        setSelectedPosition(getBishopMoves(r,c))
+        // console.log(selectedPosition)
+    }
+    return <>
+    <div>
+        <h1>Chess Board</h1>
+        {
+            chessMatrix.map((row, rowIndex) => (
+                <div className="chess_row" key={rowIndex}>
+                   {
+                    row.map((color, colIndex) => {
+                        const isMoveHighlightned = selectedPosition?.has(`${rowIndex}-${colIndex}`)
+                        // why this question mark, initially there wont be any selectedposition set thats why. we have to check. Otherwise will face issue.
+                        return (
+                            (
+                                
+                                <div className= {`chess_col ${color}`} key={colIndex} 
+                                style={{cursor:"pointer", "backgroundColor" : isMoveHighlightned ? "red" : null}} 
+                                onClick={() => handleClick(rowIndex,colIndex)}>
+                                    
+                                </div>
+                            )
+                        )
+                    })
+                   }
+                </div>
+            ))
+        }
+    </div>
+    </>
+}
