75331fba706cb575f6aa0be7114738eb22a47610
Rama Krishna <injarapuramakrishna55@gmail.com>
2025-02-28 22:21:37 +0530

    Modal feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import "./Components/Question/Question"
 import Question from "./Components/Question/Question"
 import ChessSecondApproach from "./Components/Chess/ChessSecondApproach"
 import Chess from "./Components/Chess/Chess"
+import TodoList from "./Components/TodoList/TodoList"
+import Modal from "./Components/Modal/Modal"
 export default function App() {
   return (
     <div>
-      <ChessSecondApproach />
+      <Modal />
     </div>
   )
 }
---
+++ src/Components/Modal/Modal.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import "./Modal.css";
+
+const Modal = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <div className='modal-container'>
+            {!isOpen && (
+                <div>
+                    <h1>Modal</h1>
+                    <button className='modal-button' onClick={() => setIsOpen(true)}>Click Me</button>
+                </div>
+            )}
+
+            {isOpen && (
+                <>
+                    <div className='modal-overlay' onClick={() => setIsOpen(false)}></div>
+                    <div className='modal-content'>
+                        <p>
+                            Redux Toolkit is our official recommended approach for writing Redux logic. It wraps 
+                            around the Redux core and contains packages and functions essential for building a Redux app.
+                        </p>
+                        <button className='close-button' onClick={() => setIsOpen(false)}>Close</button>
+                    </div>
+                </>
+            )}
+        </div>
+    );
+}
+
+export default Modal;
